6e7ed9c4591fd7f6d1f27dbf9b42799a767ae6a1
sbx_user1051 <sbx_user1051@169.254.32.109>
2024-04-30 22:46:58 +0000

    Tue Apr 30 22:46:58 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,19 +1,3 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-
-    try:
-        if not isinstance(x, int) or not isinstance(y, int):
-            raise ValueError("Parameter x or y is not an integer")
-        if x > 100 or x < 0 or y > 100 or y < 0:
-            raise  ValueError("Parameter x or y is not in the range 0-100 inclusive")
-    
-        return x + y
-
-    except ValueError as err:
-        print(err.args)
-    except ArithmeticError as err:
-        print(err.args)
-    except Exception as e:
-        print(e.args)
-
-    
+    raise NotImplementedError()
