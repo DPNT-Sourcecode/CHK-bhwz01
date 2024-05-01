66d5a914857041f1214549c7997221f76709eee5
sbx_user1051 <sbx_user1051@169.254.11.249>
2024-05-01 12:54:51 +0000

    Wed May 01 12:54:51 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -2,9 +2,10 @@ from lib.solutions.CHK import checkout_solution
 import pytest
 
 class TestChk():
-    def test_sum(self):
+    def test_chk(self):
         assert checkout_solution.compute("ABBBBCD") == 245
 
     
         
+
 
