289f2abe30b192ee69852debe971a2b11afd6755
sbx_user1051 <sbx_user1051@169.254.11.249>
2024-05-01 12:48:51 +0000

    Wed May 01 12:48:51 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,49 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    if not isinstance(skus, str):
+        return -1
+    
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+    basketCount = {"A": 0, "B": 0, "C": 0, "D": 0}
+
+    for sku in skus:
+        if sku in prices:
+            basketCount[sku] += 1
+
+    totalPrice = 0
+
+    for key in basketCount.keys():
+        itemFreqBasket = basketCount[key]
+
+        if key == "A" and (itemFreqBasket // 3) > 0:
+            divisible = itemFreqBasket // 3
+            remainder = itemFreqBasket - divisible
+
+            total = (divisible * 130) + remainder * prices[key]
+            totalPrice += total
+            continue
+        
+        if key == "B" and (itemFreqBasket // 2) > 0:
+            # if the user has ordered at least 2 items  
+            divisible = itemFreqBasket // 2
+            remainder = itemFreqBasket - divisible
+
+            total = (divisible * 45) + remainder * prices[key]
+            totalPrice += total
+            continue
+        
+        totalPrice += (prices[key] * itemFreqBasket)
+
+    
+    return totalPrice
+
+    
+    
+
+
+
+
