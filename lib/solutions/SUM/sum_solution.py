30eba9d8a12bf4bada0aebc6007a325042eb15eb
sbx_user1051 <sbx_user1051@169.254.56.77>
2024-05-01 08:37:46 +0000

    Wed May 01 08:37:46 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,22 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    
+    try:
+        if isinstance(x, int) or isinstance(y, int):
+            raise ValueError("Parameters x and y must both be integers.")
+
+        if x < 0 or x > 100 or y > 0 or y < 100:
+            raise ValueError("Parameters x and y need to be in the range 1")
+
+        return x + y
+    
+    except ValueError as e:
+        print(e.args)
+    except ArithmeticError as e:
+        print(e.args)
+    except Exception as e:
+        print(e.args)
+
+
+
+
