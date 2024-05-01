d00d4d8c183bf67c1590883f33a3fc6933b34d8f
sbx_user1051 <sbx_user1051@169.254.55.149>
2024-05-01 16:22:50 +0000

    Wed May 01 16:22:50 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,31 +3,28 @@ import pytest
 
 class TestChk():
     def test_chk(self):
-        # assert checkout_solution.checkout("ABBBBBCD") == 205
+        assert checkout_solution.checkout("ABBBBBCD") == 205
 
-        # assert checkout_solution.checkout("") == 0
+        assert checkout_solution.checkout("") == 0
 
-        # assert checkout_solution.checkout("AAABBCCDD") == 245
+        assert checkout_solution.checkout("AAABBCCDD") == 245
         
-        # assert checkout_solution.checkout("ABcD") == -1
+        assert checkout_solution.checkout("ABcD") == -1
 
-        # assert checkout_solution.checkout(35) == -1
+        assert checkout_solution.checkout(35) == -1
 
-        # assert checkout_solution.checkout(34.5) == -1
+        assert checkout_solution.checkout(34.5) == -1
 
-        # # ensuring that we DO NOT give the free B away
-        # assert checkout_solution.checkout("ABBBBBBEEE") == 305
+        # ensuring that we give 1 free B away
+        assert checkout_solution.checkout("ABBBBBBEEE") == 290
 
-        # # ensuring that we DO give the free B away
-        # assert checkout_solution.checkout("ABBBBBEEE") == 260
+        # case where we have multiple pairs of Es. For every pair of E we perhaps may take away one B
+        # in this case we want to take away two BS
+        assert checkout_solution.checkout("ABBBBBBEEEE") == 300
 
-        # # case where we have multiple pairs of Es. For every pair of E we perhaps may take away one B
-        # # in this case we want to take away two BS
-        # assert checkout_solution.checkout("ABBBBBBEEEE") == 300
+        assert checkout_solution.checkout("AAAAAAAA") == 330
 
-        # assert checkout_solution.checkout("AAAAAAAA") == 330
-
-        # assert checkout_solution.checkout("AAAAAAAAA") == 380
+        assert checkout_solution.checkout("AAAAAAAAA") == 380
 
         assert checkout_solution.checkout("ABCDEABCDE") == 280
 
@@ -38,5 +35,6 @@ class TestChk():
 
     
         
+
 
 
