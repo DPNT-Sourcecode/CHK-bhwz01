a20449bc73097b7c802e9f8c5d3f4af40b687aaf
sbx_user1051 <sbx_user1051@169.254.55.149>
2024-05-01 18:10:37 +0000

    Wed May 01 18:10:37 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,58 +3,62 @@ import pytest
 
 class TestChk():
     def test_chk(self):
-        # assert checkout_solution.checkout("ABBBBBCD") == 205
+        assert checkout_solution.checkout("ABBBBBCD") == 205
 
-        # assert checkout_solution.checkout("") == 0
+        assert checkout_solution.checkout("") == 0
 
-        # assert checkout_solution.checkout("AAABBCCDD") == 245
+        assert checkout_solution.checkout("AAABBCCDD") == 245
         
-        # assert checkout_solution.checkout("ABcD") == -1
+        assert checkout_solution.checkout("ABcD") == -1
 
-        # assert checkout_solution.checkout(35) == -1
+        assert checkout_solution.checkout(35) == -1
 
-        # assert checkout_solution.checkout(34.5) == -1
+        assert checkout_solution.checkout(34.5) == -1
 
-        # # ensuring that we give 1 free B away
-        # assert checkout_solution.checkout("ABBBBBBEEE") == 290
+        # ensuring that we give 1 free B away
+        assert checkout_solution.checkout("ABBBBBBEEE") == 290
 
-        # # case where we have multiple pairs of Es. For every pair of E we perhaps may take away one B
-        # # in this case we want to take away two BS
-        # assert checkout_solution.checkout("ABBBBBBEEEE") == 300
+        # case where we have multiple pairs of Es. For every pair of E we perhaps may take away one B
+        # in this case we want to take away two BS
+        assert checkout_solution.checkout("ABBBBBBEEEE") == 300
 
-        # assert checkout_solution.checkout("AAAAAAAA") == 330
+        assert checkout_solution.checkout("AAAAAAAA") == 330
 
-        # assert checkout_solution.checkout("AAAAAAAAA") == 380
+        assert checkout_solution.checkout("AAAAAAAAA") == 380
 
-        # assert checkout_solution.checkout("ABCDEABCDE") == 280
+        assert checkout_solution.checkout("ABCDEABCDE") == 280
         
-        # assert checkout_solution.checkout("AAAAAA") == 250
+        assert checkout_solution.checkout("AAAAAA") == 250
 
-        # # exactly 3 Fs in basket
-        # assert checkout_solution.checkout("FFF") == 20
+        # exactly 3 Fs in basket
+        assert checkout_solution.checkout("FFF") == 20
 
-        # # exactly 4 Fs in basket
-        # assert checkout_solution.checkout("FFFF") == 30
+        # exactly 4 Fs in basket
+        assert checkout_solution.checkout("FFFF") == 30
 
-        # # multiple of 3 Fs in basket (6)
-        # assert checkout_solution.checkout("FFFFFF") == 40
+        # multiple of 3 Fs in basket (6)
+        assert checkout_solution.checkout("FFFFFF") == 40
 
-        # # multiple of 3 Fs in basket (8)
-        # assert checkout_solution.checkout("FFFFFFFF") == 60
+        # multiple of 3 Fs in basket (8)
+        assert checkout_solution.checkout("FFFFFFFF") == 60
 
-        # # mix with Fs in basket (8)
-        # assert checkout_solution.checkout("ABBFFFFFFFF") == 155
+        # mix with Fs in basket (8)
+        assert checkout_solution.checkout("ABBFFFFFFFF") == 155
 
-        # assert checkout_solution.checkout("RRRQ") == 150
+        assert checkout_solution.checkout("RRRQ") == 150
 
         # assert checkout_solution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 965
 
-        assert checkout_solution.checkout("STXYZXXZ") == 965
+        assert checkout_solution.checkout("STXYZXXZ") == (45 + 45 + 34)
+
+        assert checkout_solution.checkout("XXXYY") == (45 + 34)
+
 
  
 
 
     
         
+
 
 
